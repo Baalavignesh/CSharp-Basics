8ce06a61d7fe9baa18094cd0194093a975df0765
Baalavignesh <baalavignesh21@gmail.com>
2023-03-21 12:15:56 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        public static void CaloriesCalculator(int x, int y)
+        {
+            Console.WriteLine(x + y);
+        }
+        static void Main(string[] args)
+        {
+            int apple = 160;
+            int orange = (int)120.2;
+
+            CaloriesCalculator(apple, orange);
+
+        }
+    }
+}
